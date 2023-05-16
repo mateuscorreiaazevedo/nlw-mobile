b38eed0a19442f77f52476d4b3b8241b3eff3498
Mateus <mateuscorreiaazevedo@gmail.com>
2023-05-16 01:21:05 -0300

    feat: init application mobile

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: '.',
+          extensions: ['.ts', '.js', '.tsx', '.jsx', 'android.js', 'android,tsx', 'ios.js', 'ios.tsx'],
+          alias: {
+            '@': './src'
+          }
+        }
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false
+        }
+      ]
+    ]
+  }
+}
